1c3609bca694b05b8452532a203db820b9560258
Jean El Khoury <sean.elkhoury@gmail.com>
2022-11-30 15:06:24 +0100

    initial commit

---
+++ createSoundgoodDB.sql
@@ -0,0 +1,280 @@
+CREATE DATABASE soundgood;
+
+\c soundgood
+
+CREATE TYPE SKILL AS ENUM ('beginner', 'intermediate', 'advanced');
+
+CREATE TABLE instrument_type (
+ instrument_type VARCHAR(100) NOT NULL
+);
+
+ALTER TABLE instrument_type ADD CONSTRAINT PK_instrument_type PRIMARY KEY (instrument_type);
+
+
+CREATE TABLE instruments_in_stock (
+ id SERIAL,
+ instrument_type VARCHAR(100) NOT NULL,
+ brand VARCHAR(100),
+ model VARCHAR(100),
+ information VARCHAR(250),
+ price DOUBLE PRECISION NOT NULL
+);
+
+ALTER TABLE instruments_in_stock ADD CONSTRAINT PK_instruments_in_stock PRIMARY KEY (id);
+
+
+CREATE TABLE person (
+ id SERIAL,
+ first_name VARCHAR(100) NOT NULL,
+ last_name VARCHAR(250) NOT NULL,
+ social_security_number  VARCHAR(12) NOT NULL UNIQUE,
+ email  VARCHAR(100) NOT NULL UNIQUE,
+ city VARCHAR(100) NOT NULL,
+ street VARCHAR(100) NOT NULL,
+ zip_code VARCHAR(5) NOT NULL,
+ house_number VARCHAR(10)
+);
+
+ALTER TABLE person ADD CONSTRAINT PK_person PRIMARY KEY (id);
+
+
+CREATE TABLE person_phone (
+ phone_number  VARCHAR(10) NOT NULL UNIQUE,
+ person_id INT NOT NULL
+);
+
+ALTER TABLE person_phone ADD CONSTRAINT PK_person_phone PRIMARY KEY (phone_number ,person_id);
+
+
+CREATE TABLE physical_instrument (
+ id SERIAL,
+ instruments_in_stock_id INT NOT NULL
+);
+
+ALTER TABLE physical_instrument ADD CONSTRAINT PK_physical_instrument PRIMARY KEY (id);
+
+
+CREATE TABLE price (
+ id SERIAL,
+ amount VARCHAR(10) NOT NULL,
+ pricing_scheme VARCHAR(250)
+);
+
+ALTER TABLE price ADD CONSTRAINT PK_price PRIMARY KEY (id);
+
+
+CREATE TABLE student (
+ id SERIAL,
+ person_id INT NOT NULL
+);
+
+ALTER TABLE student ADD CONSTRAINT PK_student PRIMARY KEY (id);
+
+
+CREATE TABLE student_instrument_skill_level (
+ student_id INT NOT NULL,
+ instrument_type VARCHAR(100) NOT NULL,
+ skill_level SKILL NOT NULL
+);
+
+ALTER TABLE student_instrument_skill_level ADD CONSTRAINT PK_student_instrument_skill_level PRIMARY KEY (student_id,instrument_type);
+
+
+CREATE TABLE time_slot (
+ id SERIAL,
+ date DATE,
+ start_time TIME(10),
+ end_time  TIME(10)
+);
+
+ALTER TABLE time_slot ADD CONSTRAINT PK_time_slot PRIMARY KEY (id);
+
+
+CREATE TABLE emergency_contact (
+ student_id INT NOT NULL,
+ student_relation VARCHAR(250),
+ phone_number  VARCHAR(10) NOT NULL UNIQUE
+);
+
+ALTER TABLE emergency_contact ADD CONSTRAINT PK_emergency_contact PRIMARY KEY (student_id);
+
+
+CREATE TABLE instructor (
+ id SERIAL,
+ person_id INT NOT NULL
+);
+
+ALTER TABLE instructor ADD CONSTRAINT PK_instructor PRIMARY KEY (id);
+
+
+CREATE TABLE instructor_instrument_type (
+ instructor_id INT NOT NULL,
+ instrument_type VARCHAR(100) NOT NULL
+);
+
+ALTER TABLE instructor_instrument_type ADD CONSTRAINT PK_instructor_instrument_type PRIMARY KEY (instructor_id,instrument_type);
+
+
+CREATE TABLE private_lesson (
+ student_id INT NOT NULL,
+ instructor_id INT NOT NULL,
+ time_slot_id INT NOT NULL,
+ instrument_type VARCHAR(100) NOT NULL,
+ price_id INT NOT NULL,
+ skill_level SKILL NOT NULL
+);
+
+ALTER TABLE private_lesson ADD CONSTRAINT PK_private_lesson PRIMARY KEY (student_id,instructor_id,time_slot_id);
+
+
+CREATE TABLE rental (
+ student_id INT NOT NULL,
+ physical_instrument_id INT NOT NULL,
+ start_date DATE NOT NULL,
+ end_date DATE NOT NULL
+);
+
+ALTER TABLE rental ADD CONSTRAINT PK_rental PRIMARY KEY (student_id,physical_instrument_id);
+
+
+CREATE TABLE sibling (
+ student_id INT NOT NULL,
+ sibling_id INT NOT NULL
+);
+
+ALTER TABLE sibling ADD CONSTRAINT PK_sibling PRIMARY KEY (student_id,sibling_id);
+
+
+CREATE TABLE ensemble (
+ id SERIAL,
+ instructor_id INT NOT NULL,
+ min_number_of_students INT NOT NULL,
+ max_number_of_students INT NOT NULL,
+ genre VARCHAR(100),
+ price_id INT NOT NULL,
+ skill_level SKILL NOT NULL
+);
+
+ALTER TABLE ensemble ADD CONSTRAINT PK_ensemble PRIMARY KEY (id);
+
+
+CREATE TABLE ensemble_instrument_type (
+ instrument_type VARCHAR(100) NOT NULL,
+ ensemble_id INT NOT NULL
+);
+
+ALTER TABLE ensemble_instrument_type ADD CONSTRAINT PK_ensemble_instrument_type PRIMARY KEY (instrument_type,ensemble_id);
+
+
+CREATE TABLE ensemble_student (
+ student_id INT NOT NULL,
+ ensemble_id INT NOT NULL
+);
+
+ALTER TABLE ensemble_student ADD CONSTRAINT PK_ensemble_student PRIMARY KEY (student_id,ensemble_id);
+
+
+CREATE TABLE ensemble_time_slot (
+ ensemble_id INT NOT NULL,
+ time_slot_id INT NOT NULL
+);
+
+ALTER TABLE ensemble_time_slot ADD CONSTRAINT PK_ensemble_time_slot PRIMARY KEY (ensemble_id,time_slot_id);
+
+
+CREATE TABLE group_lesson (
+ id SERIAL,
+ min_number_of_students INT NOT NULL,
+ max_number_of_students INT NOT NULL,
+ instrument_type VARCHAR(100) NOT NULL,
+ instructor_id INT NOT NULL,
+ price_id INT NOT NULL,
+ skill_level SKILL NOT NULL
+);
+
+ALTER TABLE group_lesson ADD CONSTRAINT PK_group_lesson PRIMARY KEY (id);
+
+
+CREATE TABLE group_lesson_student (
+ student_id INT NOT NULL,
+ group_lesson_id INT NOT NULL
+);
+
+ALTER TABLE group_lesson_student ADD CONSTRAINT PK_group_lesson_student PRIMARY KEY (student_id,group_lesson_id);
+
+
+CREATE TABLE group_lesson_time_slot (
+ group_lesson_id INT NOT NULL,
+ time_slot_id INT NOT NULL
+);
+
+ALTER TABLE group_lesson_time_slot ADD CONSTRAINT PK_group_lesson_time_slot PRIMARY KEY (group_lesson_id,time_slot_id);
+
+
+ALTER TABLE person_phone ADD CONSTRAINT FK_person_phone_0 FOREIGN KEY (person_id) REFERENCES person (id);
+
+
+ALTER TABLE physical_instrument ADD CONSTRAINT FK_physical_instrument_0 FOREIGN KEY (instruments_in_stock_id) REFERENCES instruments_in_stock (id);
+
+
+ALTER TABLE student ADD CONSTRAINT FK_student_0 FOREIGN KEY (person_id) REFERENCES person (id);
+
+
+ALTER TABLE student_instrument_skill_level ADD CONSTRAINT FK_student_instrument_skill_level_0 FOREIGN KEY (student_id) REFERENCES student (id);
+ALTER TABLE student_instrument_skill_level ADD CONSTRAINT FK_student_instrument_skill_level_1 FOREIGN KEY (instrument_type) REFERENCES instrument_type (instrument_type);
+
+
+ALTER TABLE emergency_contact ADD CONSTRAINT FK_emergency_contact_0 FOREIGN KEY (student_id) REFERENCES student (id);
+
+
+ALTER TABLE instructor ADD CONSTRAINT FK_instructor_0 FOREIGN KEY (person_id) REFERENCES person (id);
+
+
+ALTER TABLE instructor_instrument_type ADD CONSTRAINT FK_instructor_instrument_type_0 FOREIGN KEY (instructor_id) REFERENCES instructor (id);
+ALTER TABLE instructor_instrument_type ADD CONSTRAINT FK_instructor_instrument_type_1 FOREIGN KEY (instrument_type) REFERENCES instrument_type (instrument_type);
+
+
+ALTER TABLE private_lesson ADD CONSTRAINT FK_private_lesson_0 FOREIGN KEY (student_id) REFERENCES student (id);
+ALTER TABLE private_lesson ADD CONSTRAINT FK_private_lesson_1 FOREIGN KEY (instructor_id) REFERENCES instructor (id);
+ALTER TABLE private_lesson ADD CONSTRAINT FK_private_lesson_2 FOREIGN KEY (time_slot_id) REFERENCES time_slot (id);
+ALTER TABLE private_lesson ADD CONSTRAINT FK_private_lesson_3 FOREIGN KEY (instrument_type) REFERENCES instrument_type (instrument_type);
+ALTER TABLE private_lesson ADD CONSTRAINT FK_private_lesson_4 FOREIGN KEY (price_id) REFERENCES price (id);
+
+
+ALTER TABLE rental ADD CONSTRAINT FK_rental_0 FOREIGN KEY (student_id) REFERENCES student (id);
+ALTER TABLE rental ADD CONSTRAINT FK_rental_1 FOREIGN KEY (physical_instrument_id) REFERENCES physical_instrument (id);
+
+
+ALTER TABLE sibling ADD CONSTRAINT FK_sibling_0 FOREIGN KEY (student_id) REFERENCES student (id);
+ALTER TABLE sibling ADD CONSTRAINT FK_sibling_1 FOREIGN KEY (sibling_id) REFERENCES student (id);
+
+
+ALTER TABLE ensemble ADD CONSTRAINT FK_ensemble_0 FOREIGN KEY (instructor_id) REFERENCES instructor (id);
+ALTER TABLE ensemble ADD CONSTRAINT FK_ensemble_1 FOREIGN KEY (price_id) REFERENCES price (id);
+
+
+ALTER TABLE ensemble_instrument_type ADD CONSTRAINT FK_ensemble_instrument_type_0 FOREIGN KEY (instrument_type) REFERENCES instrument_type (instrument_type);
+ALTER TABLE ensemble_instrument_type ADD CONSTRAINT FK_ensemble_instrument_type_1 FOREIGN KEY (ensemble_id) REFERENCES ensemble (id);
+
+
+ALTER TABLE ensemble_student ADD CONSTRAINT FK_ensemble_student_0 FOREIGN KEY (student_id) REFERENCES student (id);
+ALTER TABLE ensemble_student ADD CONSTRAINT FK_ensemble_student_1 FOREIGN KEY (ensemble_id) REFERENCES ensemble (id);
+
+
+ALTER TABLE ensemble_time_slot ADD CONSTRAINT FK_ensemble_time_slot_0 FOREIGN KEY (ensemble_id) REFERENCES ensemble (id);
+ALTER TABLE ensemble_time_slot ADD CONSTRAINT FK_ensemble_time_slot_1 FOREIGN KEY (time_slot_id) REFERENCES time_slot (id);
+
+
+ALTER TABLE group_lesson ADD CONSTRAINT FK_group_lesson_0 FOREIGN KEY (instrument_type) REFERENCES instrument_type (instrument_type);
+ALTER TABLE group_lesson ADD CONSTRAINT FK_group_lesson_1 FOREIGN KEY (instructor_id) REFERENCES instructor (id);
+ALTER TABLE group_lesson ADD CONSTRAINT FK_group_lesson_2 FOREIGN KEY (price_id) REFERENCES price (id);
+
+
+ALTER TABLE group_lesson_student ADD CONSTRAINT FK_group_lesson_student_0 FOREIGN KEY (student_id) REFERENCES student (id);
+ALTER TABLE group_lesson_student ADD CONSTRAINT FK_group_lesson_student_1 FOREIGN KEY (group_lesson_id) REFERENCES group_lesson (id);
+
+
+ALTER TABLE group_lesson_time_slot ADD CONSTRAINT FK_group_lesson_time_slot_0 FOREIGN KEY (group_lesson_id) REFERENCES group_lesson (id);
+ALTER TABLE group_lesson_time_slot ADD CONSTRAINT FK_group_lesson_time_slot_1 FOREIGN KEY (time_slot_id) REFERENCES time_slot (id);
+
+
---
+++ insertSoundgoodData.sql
@@ -0,0 +1,147 @@
+INSERT INTO person (first_name,last_name,social_security_number,email,city,street,zip_code)
+VALUES
+  ('Kieran','Goff',8316,'pede@protonmail.ca','Biên Hòa','Ap #692-737 Rhoncus. Street','44698'),
+  ('Jacob','Cole',4595,'dolor.nonummy.ac@google.couk','Sagamu','4959 Fringilla St.','46499'),
+  ('Rogan','Briggs',1804,'varius.ultrices.mauris@google.com','San Rafael','Ap #196-454 At, St.','46749'),
+  ('Theodore','Moody',2870,'eleifend.non@icloud.org','Simon''s Town','6522 Nulla Avenue','43812'),
+  ('Kato','Perry',3472,'pharetra.sed@yahoo.com','Couthuin','Ap #453-192 Libero. St.','30513'),
+  ('John','Mccall',6207,'massa@protonmail.net','Belfast','3187 Vitae, St.','19454'),
+  ('Armando','Daugherty',5741,'metus.sit.amet@protonmail.edu','Tejar','140-9550 Dolor Av.','53666'),
+  ('Mark','Dawson',4081,'vivamus.sit.amet@google.couk','Ila','413 Integer Street','39347'),
+  ('Odysseus','Wolf',9904,'nulla.interdum.curabitur@yahoo.couk','Rzeszów','914-8071 Facilisis Rd.','74843'),
+  ('Melvin','Hendricks',8664,'quisque@icloud.net','Sungei Kadut','Ap #697-7291 Mauris St.','29154');
+INSERT INTO person (first_name,last_name,social_security_number,email,city,street,zip_code)
+VALUES
+  ('Keefe','Jordan',9577,'tellus.aenean.egestas@google.org','Lambayeque','162-3276 Purus. St.','72487'),
+  ('Nasim','Thompson',6089,'urna.justo.faucibus@yahoo.org','San José de Alajuela','924-9834 Sed St.','15957'),
+  ('Noah','Burch',7744,'fusce.mi@protonmail.com','Borlänge','792 Vitae Rd.','68584'),
+  ('Travis','Cardenas',4507,'mi.eleifend@protonmail.ca','Gimcheon','5473 Ac Road','37768'),
+  ('Kenyon','Gutierrez',9212,'dapibus@hotmail.ca','Avesta','P.O. Box 790, 3882 Justo Rd.','84238'),
+  ('Fuller','Lane',6103,'ultrices.sit@hotmail.edu','Suncheon','574-5631 Elementum Ave','72470'),
+  ('Amal','Tyson',1193,'orci.lacus.vestibulum@hotmail.couk','Katowice','615-2421 Gravida. St.','73145'),
+  ('Noble','Wheeler',1152,'sodales@icloud.ca','Gorzów Wielkopolski','8698 Nec, Road','18547'),
+  ('Upton','Wilkinson',6700,'cras@outlook.net','Shigar','Ap #893-6459 Elementum St.','19400'),
+  ('Vladimir','Mcintosh',8602,'curabitur.egestas@outlook.couk','Bauchi','819-9913 In Avenue','88878');
+INSERT INTO person (first_name,last_name,social_security_number,email,city,street,zip_code)
+VALUES
+  ('Nathan','Rogers',2339,'mollis.dui@outlook.couk','Fremantle','P.O. Box 100, 1907 Tincidunt St.','45936'),
+  ('Carson','Hale',6421,'nec.leo.morbi@aol.edu','Baybay','907-6149 Mauris Street','40662'),
+  ('Octavius','Berg',3583,'convallis.ligula@google.edu','Hohenems','4063 Ac Av.','11348'),
+  ('Clark','Powell',8802,'sit.amet.massa@google.org','Miraflores','Ap #392-7001 Sed St.','71102'),
+  ('Tucker','Lancaster',7288,'non.luctus.sit@hotmail.net','Oaxaca','1934 Lobortis Av.','62627'),
+  ('Oleg','Langley',1457,'non.dapibus@protonmail.ca','Nam Giang','Ap #312-5100 Eget St.','70964'),
+  ('Stewart','Moreno',6070,'metus@protonmail.ca','Bremen','P.O. Box 300, 3259 At, St.','72024'),
+  ('Ashton','Roman',6658,'nec.luctus@aol.edu','Colchester','Ap #650-9590 Porttitor Road','51430'),
+  ('Jerry','Yates',8934,'placerat.cras.dictum@outlook.edu','Pishin Valley','978-3324 Lacus Road','72614'),
+  ('Russell','Buchanan',2735,'sapien@outlook.org','Hamoir','Ap #230-1624 Vel Avenue','31704');
+INSERT INTO person (first_name,last_name,social_security_number,email,city,street,zip_code)
+VALUES
+  ('Cyrus','Dawson',3017,'dolor.nonummy@google.org','Aguacaliente (San Francisco]','Ap #196-8495 Ante Street','10515'),
+  ('Ray','Lane',3666,'diam.eu.dolor@outlook.com','Palmerston','205-7396 Malesuada Rd.','39132'),
+  ('Chadwick','Nielsen',5810,'libero.proin@protonmail.com','Punggol','P.O. Box 121, 5524 Phasellus Street','24924'),
+  ('Quentin','Brooks',6264,'donec.vitae@icloud.org','Long Xuyên','686-920 Ac Rd.','73000'),
+  ('Mark','Leach',2053,'vestibulum.accumsan@outlook.ca','Pagadian','380-2105 Augue Avenue','38182'),
+  ('Drake','Mathis',2324,'nascetur.ridiculus@outlook.net','Oslo','568-8443 Mauris Rd.','41888'),
+  ('Christian','Frazier',8615,'adipiscing.elit.aliquam@icloud.ca','Bastia','826-8716 Et, Street','53182'),
+  ('Abbot','Sweet',2589,'duis.gravida.praesent@google.couk','Gallodoro','663-5704 Justo. Av.','56718'),
+  ('Amery','Houston',4851,'nulla@yahoo.couk','Da Lat','Ap #242-7217 Cursus Rd.','04411'),
+  ('Ethan','Phelps',6150,'pede.nunc.sed@aol.net','Kinross','P.O. Box 893, 5325 Faucibus Ave','64038');
+INSERT INTO person (first_name,last_name,social_security_number,email,city,street,zip_code)
+VALUES
+  ('Nero','Ryan',3894,'senectus.et@icloud.net','Digos','3383 Consequat Rd.','15545'),
+  ('Garrett','Chase',3386,'adipiscing.elit@yahoo.org','San Juan de Pasto','Ap #326-7290 Leo. Street','83456'),
+  ('Ashton','Erickson',9418,'nunc.in@yahoo.com','Białystok','Ap #570-6493 Volutpat St.','64673'),
+  ('Driscoll','Nieves',8817,'cursus.et.magna@hotmail.ca','Irpin','Ap #476-3999 Quisque Street','41733'),
+  ('William','Baird',1458,'erat.eget@icloud.com','Bauchi','591-2755 Metus Street','43250'),
+  ('Reece','Larson',7814,'auctor.quis@google.ca','Ukkel','Ap #748-9076 Eros Street','83252'),
+  ('Aristotle','Conway',2065,'non@google.com','Kalgoorlie-Boulder','Ap #315-9567 Neque St.','56373'),
+  ('Victor','Zamora',1537,'fusce.aliquet@protonmail.org','Bayeux','3960 Magna. St.','01113'),
+  ('Upton','Hampton',3638,'euismod.in@hotmail.net','Elgin','7535 Cursus Road','50251'),
+  ('Rooney','Taylor',8231,'etiam.ligula.tortor@yahoo.couk','Semarang','Ap #124-7549 Felis Avenue','17337');
+INSERT INTO person (first_name,last_name,social_security_number,email,city,street,zip_code)
+VALUES
+  ('Dolan','Morton',7175,'cursus@aol.com','Randburg','565-940 Et Street','88773'),
+  ('Felix','Banks',2597,'faucibus.orci@protonmail.ca','Lanark','Ap #240-7243 Ac, Road','49754'),
+  ('Thor','Lambert',6257,'nunc.mauris@google.ca','Cusco','Ap #110-6494 Risus. Av.','20764'),
+  ('Drew','Fuentes',6040,'cum.sociis@protonmail.ca','Kimberley','959-3258 Risus Avenue','31179'),
+  ('Jonas','Sykes',5406,'at.pretium.aliquet@yahoo.edu','Banda Aceh','431-7961 Sollicitudin Rd.','81832'),
+  ('Baxter','Maddox',2285,'vulputate.posuere.vulputate@icloud.net','Jeju','P.O. Box 126, 4436 Ligula. St.','62323'),
+  ('Blaze','Chan',2846,'nulla@protonmail.org','Gwangyang','857-7054 Non St.','17813'),
+  ('Benjamin','Frye',8522,'tempus.scelerisque@icloud.net','Krugersdorp','885-5316 Dui, Av.','97083'),
+  ('Levi','Middleton',2610,'orci@hotmail.com','Tengah','Ap #362-4124 Luctus. Av.','74777'),
+  ('Jerry','Sanford',4303,'mollis.dui@yahoo.com','Kursk','Ap #284-155 Nulla Av.','24545');
+INSERT INTO person (first_name,last_name,social_security_number,email,city,street,zip_code)
+VALUES
+  ('Tad','Cabrera',7207,'diam@hotmail.net','Limoges','379-6261 Pellentesque Road','55361'),
+  ('Ronan','Frost',3242,'ante@protonmail.com','Santa Inês','773-7658 Sed Street','06873'),
+  ('Xander','Ellis',8604,'aliquam@outlook.edu','Turbo','809-5258 Proin Av.','26248'),
+  ('Brett','Cleveland',1604,'penatibus.et@protonmail.com','Vierzon','P.O. Box 688, 3491 Ligula St.','75127'),
+  ('Gray','Gamble',6509,'etiam.imperdiet@yahoo.com','Kano','Ap #333-641 Auctor Rd.','40727'),
+  ('Alec','Rowe',4661,'interdum.feugiat@hotmail.ca','Mérida','986-159 Dignissim Avenue','21338'),
+  ('Baker','Klein',7914,'duis.ac@outlook.org','Nasirabad','415-3998 Ante Road','78727'),
+  ('Gray','Mcdonald',2920,'orci.ut@icloud.net','Vernon','Ap #221-5672 Morbi Ave','63261'),
+  ('Omar','Branch',7551,'lorem.ipsum@yahoo.ca','Sechura','Ap #815-6484 Donec St.','73607'),
+  ('Carson','Shaffer',9299,'eu.ligula.aenean@protonmail.com','Rivne','802-8165 At Ave','40666');
+INSERT INTO person (first_name,last_name,social_security_number,email,city,street,zip_code)
+VALUES
+  ('Jermaine','Bentley',8204,'primis.in.faucibus@aol.edu','Bauchi','Ap #496-9165 Phasellus Road','62035'),
+  ('Chester','Castaneda',9271,'amet.ante@outlook.net','Tula','Ap #625-4539 Parturient St.','43516'),
+  ('Graham','Benton',2950,'dolor.sit@icloud.com','Great Falls','Ap #753-4992 Lectus Av.','75767'),
+  ('Ezra','Glover',8313,'elit@icloud.com','Hunan','Ap #630-3520 Et, St.','47568'),
+  ('Judah','Mitchell',6127,'vivamus.euismod@aol.edu','Rustenburg','Ap #804-7500 Augue Rd.','16781'),
+  ('Griffith','Wolfe',8001,'tincidunt.adipiscing.mauris@aol.couk','Kalisz','Ap #869-5650 Cursus Street','18551'),
+  ('Declan','Rollins',3566,'urna.justo@outlook.com','Drohobych','8648 Ut, St.','41645'),
+  ('Barry','Tillman',2298,'purus.in.molestie@icloud.com','Kristiansund','P.O. Box 570, 3319 Non Rd.','60711'),
+  ('Damian','Eaton',3203,'tincidunt.congue.turpis@hotmail.edu','Carluke','285-7215 Faucibus St.','86551'),
+  ('Carl','Mason',4607,'ac.metus.vitae@hotmail.edu','Surbo','211-2278 Semper Avenue','55101');
+INSERT INTO person (first_name,last_name,social_security_number,email,city,street,zip_code)
+VALUES
+  ('Neil','Hull',3000,'egestas.lacinia@icloud.net','Trujillo','P.O. Box 773, 4129 Nulla Avenue','78562'),
+  ('Nasim','Gilbert',6274,'elit@icloud.com','Sooke','1955 Sit St.','47273'),
+  ('Salvador','Douglas',3537,'blandit.viverra@protonmail.edu','Besançon','Ap #946-4500 Pede Avenue','74613'),
+  ('Raphael','Solomon',7823,'in.scelerisque@google.org','Seogwipo','310-6153 Tempus Road','81935'),
+  ('Ferdinand','Stout',6282,'vel.lectus@yahoo.couk','Zhytomyr','Ap #833-7520 Gravida Avenue','14954'),
+  ('Harper','Potter',3647,'nec.malesuada.ut@hotmail.com','Dangjin','Ap #195-965 Sed St.','95328'),
+  ('Norman','Peters',6718,'ante@outlook.edu','Galway','6940 Orci. Av.','01675'),
+  ('Lev','Spence',7739,'aliquam.eu@protonmail.ca','Oberwart','8698 In St.','57818'),
+  ('Mannix','Jacobs',3189,'faucibus.orci@hotmail.edu','Rustenburg','157-1374 Aliquam Rd.','80224'),
+  ('Cooper','Bonner',4870,'ipsum.porta.elit@yahoo.ca','Trollhättan','Ap #171-8360 Nibh Rd.','77769');
+INSERT INTO person (first_name,last_name,social_security_number,email,city,street,zip_code)
+VALUES
+  ('Boris','Rose',2339,'sit.amet.faucibus@google.ca','Cork','P.O. Box 595, 8558 Urna Av.','56186'),
+  ('Gavin','Livingston',7017,'tristique.pellentesque.tellus@google.edu','Juárez','Ap #535-6382 Nisi Ave','73084'),
+  ('Paul','Hebert',2347,'non.luctus@yahoo.com','Barranca','892-2009 Ac Rd.','06137'),
+  ('Wylie','Meyers',6485,'sed.eu.eros@protonmail.edu','Märsta','8504 Malesuada Street','41891'),
+  ('Xenos','Butler',2615,'mi.aliquam@google.ca','Paita','Ap #443-9053 At Road','28658'),
+  ('Harrison','Hewitt',4803,'suspendisse.ac@google.net','Port Harcourt','291 Rutrum Street','43132'),
+  ('Kamal','Berger',7065,'in.consequat.enim@protonmail.couk','Meppel','751-7488 Donec Street','65640'),
+  ('Leo','Witt',5282,'laoreet.posuere@google.net','Irpin','Ap #455-3832 Proin Street','27842'),
+  ('Castor','Park',5834,'fusce.dolor@aol.org','River Valley','438-3131 Sed Rd.','72971'),
+  ('Jack','Scott',8029,'quis.arcu@icloud.com','Passi','Ap #776-2995 Eu, Street','10643');
+
+INSERT INTO instrument_type(instrument_type)
+VALUES
+('piano'),
+('guitar'),
+('bass'),
+('trombone'),
+('flute'),
+('drums');
+
+INSERT INTO student(person_id)
+SELECT id FROM person WHERE person.id>10;
+
+INSERT INTO instructor(person_id)
+SELECT id FROM person WHERE person.id<=10;
+
+INSERT INTO instructor_instrument_type(instructor_id, instrument_type)
+VALUES
+(1 ,'piano'),
+(2 ,'piano'),
+(1 ,'guitar');
+
+INSERT INTO student_instrument_skill_level(student_id, instrument_type, skill_level)
+VALUES
+(39 ,'piano', 'beginner'),
+(39 ,'drums', 'advanced'),
+(39 ,'guitar', 'beginner');
